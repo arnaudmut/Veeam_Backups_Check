7be679977bb2d0dafb838c05dd059b4ba35ae046
kardudu <kevin.donaldy@gmail.com>
2018-09-07 17:58:03 +0200

    function isRPOValid

--- Veeam_Backups_Check.ps1
+++ Veeam_Backups_Check.ps1
@@ -40,17 +40,17 @@
 #---------------------------------------------------------[Script Parameters]------------------------------------------------------
 
 [CmdletBinding()]
-    param(
-    [Parameter(Mandatory=$true,Position=0)]
+param(
+    [Parameter(Mandatory = $true, Position = 0)]
     $name = $args[0],
-    [Parameter(Position=1)]
+    [Parameter(Position = 1)]
     $period = $args[1],
     #Location of Veeam executable (Veeam.Backup.Shell.exe)
     $veeamExePath = "C:\Program Files\Veeam\Backup and Replication\Backup\Veeam.Backup.Shell.exe",
     $Server = "localhost"
-    )
+)
 
-    #---------------------------------------------------------[Initialisations]--------------------------------------------------------
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
 
 #region Connect
 # Load required Snapins and Modules
@@ -80,6 +80,8 @@ $job
 #$state
 #last known successful session
 $lastOkSession 
+#get today date 
+$now = (Get-Date)
 #endregion variables 
 
 #----------------------------------------------------------[Functions]----------------------------------------------------------
@@ -87,22 +89,56 @@ $lastOkSession
 #region Function
 
 Function Get-VeeamVersion {
-    Try {
-        $veeamExe = Get-Item $veeamExePath
-        $VeeamVersion = $veeamExe.VersionInfo.ProductVersion
-        Return $VeeamVersion
+    Begin {}
+    Process {
+        Try {
+            $veeamExe = Get-Item $veeamExePath
+            $VeeamVersion = $veeamExe.VersionInfo.ProductVersion
+            Return $VeeamVersion
+        }
+        Catch {
+            Write-Host "Unable to Locate Veeam executable, check path - $veeamExePath" -ForegroundColor Red
+            exit  3 
+        }
     }
-    Catch {
-        Write-Host "Unable to Locate Veeam executable, check path - $veeamExePath" -ForegroundColor Red
-        exit  3 
-    }
+    End {}
 }
 
 # Convert mode (timeframe) to hours
-function RTO {
-}
+function IsRPOValid {
+    <#
+    .SYNOPSIS
+    Function  that calculate timespan
+    .DESCRIPTION
+    This function compares two dates and returns true if [param] date1 is equal or greater than rpo  
+    .EXAMPLE
+    RPO([dateTime]date)
+    .PARAMETER date1
+    The date to compare.
+    #>
+    [CmdletBinding()]
+    param
+    (
+      [Parameter(Mandatory=$True,ValidateNotnullOrEmpty, Position = 0)]
+      [datetime]$date1
+    )
+    begin {
+    write-verbose "declaring variables"
+     $rpo = $now.AddDays(-$period)
+     $timeSpan = New-TimeSpan -Start $date1.Date -End  $rpo.Date
+    }
+    process {
+  
+      write-verbose "Beginning process comparison between the two "
+      $timeSpan -ge 0
+    }
+    end{}
+  }
 
 #endRegion Function
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
 #region Report
 $job = Get-VBRJob -Name $name
 if ($null -eq $job) {
@@ -130,13 +166,16 @@ switch ($state) {
         exit 0
     }
     idle {
-        #recovery time objective 
-        $rto = (Get-Date).AddDays(-$period)
-        if ((get-date $lastOkSession.EndTime) -ge (get-date $rto) -and ($lastOkSession.State -ne "failed")) {
+        #Recovery Point Objective 
+        $rpo
+         = (Get-Date).AddDays(-$period)
+        if ((get-date $lastOkSession.EndTime) -ge (get-date $rpo
+        ) -and ($lastOkSession.State -ne "failed")) {
             Write-Host "OK! $name waiting for new restore points."
             exit 0
         } 
-        elseif ((get-date $lastOkSession.EndTime) -lt (get-date $rto) -and ($lastOkSession.State -ne "failed")) {
+        elseif ((get-date $lastOkSession.EndTime) -lt (get-date $rpo
+        ) -and ($lastOkSession.State -ne "failed")) {
             Write-Host ("CRITICAL! the last successful session is {0} day old." -f $(Get-Date $lastOkSession.EndTime))
             exit 2
         }
@@ -146,13 +185,15 @@ switch ($state) {
         }
     }
     stopped {
-        #recovery time objective 
-        $rto = (Get-Date).AddDays(-$period)
-        if ((get-date $lastOkSession.EndTime) -gt (get-date $rto) -and ($lastOkSession.State -ne "failed")) {
+        #Recovery Point Objective 
+        $rpo = (Get-Date).AddDays(-$period)
+        if ((get-date $lastOkSession.EndTime) -gt (get-date $rpo
+        ) -and ($lastOkSession.State -ne "failed")) {
             Write-Host "OK! $name waiting for new restore points."
             exit 0
         } 
-        elseif ((get-date $lastOkSession.EndTime) -le (get-date $rto) -and ($lastOkSession.State -ne "failed")) {
+        elseif ((get-date $lastOkSession.EndTime) -le (get-date $rpo
+        ) -and ($lastOkSession.State -ne "failed")) {
             Write-Host ("CRITICAL! the last successful session is {0} days old." -f $((Get-Date $lastOkSession.EndTime).Day))
             exit 2
         }
